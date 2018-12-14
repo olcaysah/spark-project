63606f70997490c977df6c6271410c075ff4a88e
olcaysah <osahi001@odu.edu>
2018-12-13 22:54:16 -0500

    Continue to setup workspace

--- Setup Workspace.R
+++ Setup Workspace.R
@@ -7,6 +7,18 @@
 
 ##LOADING PACKAGES ###
 is.installed <- function(mypkg) is.element(mypkg, installed.packages()[,1])
-pkglist <- c('readr','dplyr','data.table','stringr','ggplot2','sparklyR')
+pkglist <- c('readr','dplyr','data.table','stringr','ggplot2','sparklyr')
 for(p in pkglist) if(is.installed(p)==F) install.packages(p, character.only=T)
 for(p in pkglist) require(p, character.only=T) 
+
+#spark_available_versions() #see available versions of spark
+spark_install(version = "2.3.2") #It was 2.4.0 version when I install spark.
+
+#Connecto to spark:
+sc <- spark_connect(master = "local")
+
+
+#Read the data:
+didi_tbl <- copy_to(sc, read_csv("C:/Users/Miray/GitHub_osahi001/DIDI-Challenge/didi_5min_ave_by_day.csv", col_types = cols()))
+
+didi_tbl %>% filter(direction == 'N')
